89ab57334994e56e3592161e41003619444c8fcf
DEVFAT <DEVFAT>
2021-08-24 23:12:10 +0700

    add

--- lib/view/HomeRandom.dart
+++ lib/view/HomeRandom.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:shake/shake.dart';
 import 'package:test_myapp/components/ItemMenuComponent.dart';
 import 'package:test_myapp/controller/MenuController.dart';
 import 'package:test_myapp/model/menu_model.dart';
+
+
 
 class HomeRandom extends StatefulWidget {
   const HomeRandom({ Key? key }) : super(key: key);
@@ -13,6 +16,10 @@ class HomeRandom extends StatefulWidget {
 class _HomeRandomState extends State<HomeRandom> {
 
   final menuController = MenuController();
+  late ShakeDetector detector;
+
+  int shakeCounter = 0;
+  final int shakeShouldRandom = 3;
 
   Future<Map<String, MenuItemModel>?> _random() async {
     try {
@@ -22,6 +29,22 @@ class _HomeRandomState extends State<HomeRandom> {
     } catch (e) {
       return null;
     }
+  }
+
+  _shakeHandle() async {
+    shakeCounter += 1;
+    if (shakeCounter >= shakeShouldRandom) {
+      await this._random();
+      shakeCounter = 0;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    detector = ShakeDetector.autoStart(
+      onPhoneShake: _shakeHandle
+    );
   }
 
   @override
@@ -49,24 +72,26 @@ class _HomeRandomState extends State<HomeRandom> {
             if (snapshot.connectionState == ConnectionState.done) {
               final menuItem = snapshot.data as Map<String, MenuItemModel>;
               return Container(
-                margin: EdgeInsets.all(80),
-                child: ItemMenuComponent(
-                  docId: menuItem.keys.first,
-                  menuItem: menuItem.values.first,
+                padding: EdgeInsets.all(50),
+                child: Column(
+                  children: [
+                    ItemMenuComponent(
+                      docId: menuItem.keys.first,
+                      menuItem: menuItem.values.first,
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {});
+                      }, 
+                      child: Text("Random")
+                    )
+                  ]
                 )
               );
             }
-
             return Container();
           },
-        ),
-
-        SizedBox(height: 10),
-        ElevatedButton(
-          onPressed: () {
-            setState(() {});
-          }, 
-          child: Text("Random")
         )
       ],
     );
